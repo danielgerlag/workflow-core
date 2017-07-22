236c8e7852e4cb8a9e5471f65f25b3b687d303ce
Daniel Gerlag <daniel@gerlag.co.za>
2017-07-22 10:55:55 -0700

    package versions

--- src/samples/WorkflowCore.Sample13/Program.cs
+++ src/samples/WorkflowCore.Sample13/Program.cs
@@ -30,7 +30,7 @@ namespace WorkflowCore.Sample13
             services.AddLogging();
             services.AddWorkflow();
             //services.AddWorkflow(x => x.UseMongoDB(@"mongodb://localhost:27017", "workflow-test002"));
-            //services.AddWorkflow(x => x.UseSqlServer(@"Server=.\SQLEXPRESS;Database=WorkflowCoreTest001;Trusted_Connection=True;", true, true));
+            //services.AddWorkflow(x => x.UseSqlServer(@"Server=.\SQLEXPRESS;Database=WorkflowCoreTest007;Trusted_Connection=True;", true, true));
             //services.AddWorkflow(x => x.UseSqlite(@"Data Source=wfc001.db;", true));            
 
             //services.AddWorkflow(x =>
