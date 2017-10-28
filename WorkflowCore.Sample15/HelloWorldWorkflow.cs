9ea9b9a05e2c19f1c98214f0f1ae0f1c70b51ab9
Daniel Gerlag <daniel@gerlag.ca>
2017-10-28 14:28:58 -0700

    Dependency injection sample

---
+++ WorkflowCore.Sample15/HelloWorldWorkflow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+using WorkflowCore.Sample15.Steps;
+
+namespace WorkflowCore.Sample15
+{
+    public class HelloWorldWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder<object> builder)
+        {
+            builder                
+                .StartWith<HelloWorld>()
+                .Then<DoSomething>();
+        }
+
+        public string Id => "HelloWorld";
+            
+        public int Version => 1;
+                 
+    }
+}
