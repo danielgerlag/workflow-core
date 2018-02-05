b81bc98b9dda5ea0cd2fb51c3cc264b684b4b362
Roberto Paterlini <r.paterlini@almaviva.it>
2018-02-05 11:37:36 +0100

    Fact moved to BaseQueueProviderFixture

--- test/WorkflowCore.Tests.SqlServer/SqlServerQueueProviderFixture.cs
+++ test/WorkflowCore.Tests.SqlServer/SqlServerQueueProviderFixture.cs
@@ -1,20 +1,12 @@
 ﻿#region using
 
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-
-using FluentAssertions;
 
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
-using WorkflowCore.Interface;
 using WorkflowCore.QueueProviders.SqlServer.Services;
+using WorkflowCore.UnitTests;
 
 using Xunit;
 using Xunit.Abstractions;
@@ -24,33 +16,21 @@ using Xunit.Abstractions;
 namespace WorkflowCore.Tests.SqlServer
 {
     [Collection("SqlServer collection")]
-    public class SqlServerQueueProviderFixture : IDisposable
+    public class SqlServerQueueProviderFixture : BaseQueueProviderFixture, IDisposable
     {
-        #region Init
-
-        private readonly SqlServerQueueProvider _qb;
-        private readonly ITestOutputHelper _console;
         private static ServiceProvider _serviceProvider;
 
         public SqlServerQueueProviderFixture(ITestOutputHelper output, SqlDockerSetup setup)
         {
-            _console = output;
+            Console = output;
             var connectionString = SqlDockerSetup.ConnectionString;
             ConfigureServices();
 
             var opt = new SqlServerQueueProviderOption(connectionString, "UnitTest", true, true);
-            _qb = new SqlServerQueueProvider(_serviceProvider, opt);
-            _qb.Start().Wait();
+            QueueProvider = new SqlServerQueueProvider(_serviceProvider, opt);
+            QueueProvider.Start().Wait();
 
-            while (_qb.DequeueWork(QueueType.Event, CancellationToken.None).Result != null)
-            {
-                // Empty queue before test
-            }
-
-            while (_qb.DequeueWork(QueueType.Workflow, CancellationToken.None).Result != null)
-            {
-                // Empty queue before test
-            }
+            Setup();
         }
 
         private static void ConfigureServices()
@@ -61,98 +41,7 @@ namespace WorkflowCore.Tests.SqlServer
 
         public void Dispose()
         {
-            _qb.Dispose();
-        }
-
-        #endregion
-
-        #region QueueDeque
-
-        [Fact]
-        public void ShouldEnqueueAndDequeueAMessage()
-        {
-            var id = Guid.NewGuid().ToString();
-
-            DoTest(id, QueueType.Event);
-
-            id = Guid.NewGuid().ToString();
-            DoTest(id, QueueType.Workflow);
-        }
-
-        void DoTest(string id, QueueType queueType)
-        {
-            _qb.QueueWork(id, queueType).Wait();
-            var res = _qb.DequeueWork(queueType, CancellationToken.None).Result;
-
-            res.Should().Be(id);
-        }
-
-        #endregion
-
-        [Fact]
-        public void ShouldEnqueueAndDequeueManyMessageOnManyThread()
-        {
-            const int countEvent = 250;
-            const int countThread = 10;
-            const QueueType queueType = QueueType.Event;
-
-            var guids = new ConcurrentDictionary<string, int>();
-
-
-            bool stop = false;
-            var sw = Stopwatch.StartNew();
-
-            _console.WriteLine("Start dequeue task");
-            var thDeque = new List<Task>();
-            for (int i = 0; i < countThread; i++)
-            {
-                Task t = Task.Factory.StartNew(() =>
-                {
-                    _console.WriteLine("-> Dequeue task " + Task.CurrentId);
-                    while (!stop)
-                    {
-                        var id = _qb.DequeueWork(queueType, CancellationToken.None).Result;
-                        if (id != null) guids.AddOrUpdate(id, 0, (key, oldval) => oldval + 1);
-                    }
-
-                    _console.WriteLine("<- Dequeue task " + Task.CurrentId);
-                });
-                thDeque.Add(t);
-            }
-
-            _console.WriteLine("Start enqueue task");
-            var thEnque = new List<Task>();
-            for (int i = 0; i < countThread; i++)
-            {
-                Task t = Task.Factory.StartNew(() =>
-                {
-                    _console.WriteLine("-> Enqueue task " + Task.CurrentId);
-
-                    for (int j = 0; j < countEvent; j++)
-                    {
-                        var guid = Guid.NewGuid().ToString();
-                        guids.TryAdd(guid, 0);
-                        _qb.QueueWork(guid, queueType).Wait();
-                    }
-
-                    _console.WriteLine("<- Enqueue task " + Task.CurrentId);
-                });
-                thEnque.Add(t);
-            }
-
-            Task.WaitAll(thEnque.ToArray());
-            _console.WriteLine("Enqueue complete " + sw.ElapsedMilliseconds + " msec");
-
-            stop = true;
-            Task.WaitAll(thDeque.ToArray());
-            _console.WriteLine("Dequeue complete " + sw.ElapsedMilliseconds + " msec");
-
-            foreach (var guid in guids)
-            {
-                guid.Value.Should().Be(1);
-            }
-
-            _console.WriteLine("MultiTest complete " + (guids.Count/(sw.ElapsedMilliseconds/1000.0)) + " msg/sec");
+            QueueProvider.Dispose();
         }
     }
 }
