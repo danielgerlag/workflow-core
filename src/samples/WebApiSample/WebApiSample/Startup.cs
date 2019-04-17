ba365ad2b0113b1524562d24a7b12fbbeaa875dd
Hieu Do <xlu.untitled@gmail.com>
2019-04-17 10:16:04 +0700

    chore(sample): Add Swagger for WebApiSample

--- src/samples/WebApiSample/WebApiSample/Startup.cs
+++ src/samples/WebApiSample/WebApiSample/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Nest;
+using Swashbuckle.AspNetCore.Swagger;
 using WebApiSample.Workflows;
 using WorkflowCore.Interface;
 
@@ -27,6 +28,13 @@ namespace WebApiSample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
             services.AddWorkflow(cfg =>
             {
                 cfg.UseMongoDB(@"mongodb://mongo:27017", "workflow");
@@ -40,6 +48,16 @@ namespace WebApiSample
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
 
