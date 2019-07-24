ddfd7e96419a129c5acff0753497bdd4ceeba092
Poorya <poorya.mohammadi@solera.nl>
2019-07-24 17:46:22 +0200

    parameterize postgres provider schema name

--- src/providers/WorkflowCore.Persistence.PostgreSQL/MigrationContextFactory.cs
+++ src/providers/WorkflowCore.Persistence.PostgreSQL/MigrationContextFactory.cs
@@ -7,7 +7,7 @@ namespace WorkflowCore.Persistence.PostgreSQL
     {
         public PostgresContext CreateDbContext(string[] args)
         {
-            return new PostgresContext(@"Server=127.0.0.1;Port=5432;Database=workflow;User Id=postgres;Password=password;");
+            return new PostgresContext(@"Server=127.0.0.1;Port=5432;Database=workflow;User Id=postgres;Password=password;","wfc");
         }
     }
 }
--- src/providers/WorkflowCore.Persistence.PostgreSQL/PostgresContextFactory.cs
+++ src/providers/WorkflowCore.Persistence.PostgreSQL/PostgresContextFactory.cs
@@ -9,15 +9,17 @@ namespace WorkflowCore.Persistence.PostgreSQL
     public class PostgresContextFactory : IWorkflowDbContextFactory
     {
         private readonly string _connectionString;
+        private readonly string _schemaName;
 
-        public PostgresContextFactory(string connectionString)
+        public PostgresContextFactory(string connectionString, string schemaName)
         {
             _connectionString = connectionString;
+            _schemaName = schemaName;
         }
 
         public WorkflowDbContext Build()
         {
-            return new PostgresContext(_connectionString);
+            return new PostgresContext(_connectionString,_schemaName);
         }
     }
 }
