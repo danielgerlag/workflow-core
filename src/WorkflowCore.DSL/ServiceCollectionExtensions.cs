7e07419a9672e534626b5196aabac2fec883104b
Daniel Gerlag <daniel@gerlag.ca>
2019-12-21 14:06:51 -0800

    Split DSL into own package

---
+++ src/WorkflowCore.DSL/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using WorkflowCore.Interface;
+using WorkflowCore.Services.DefinitionStorage;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddWorkflowDSL(this IServiceCollection services)
+        {
+            services.AddTransient<IDefinitionLoader, DefinitionLoader>();
+            return services;
+        }
+    }
+}
+
