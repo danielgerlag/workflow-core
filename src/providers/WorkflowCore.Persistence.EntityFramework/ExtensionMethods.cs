057d19ce613889baf7f2d03bb5f59daf0c9b1e7e
Othman Fayed <o.fayed@esdm.aero>
2020-01-09 20:37:50 +0200

    null check added

--- src/providers/WorkflowCore.Persistence.EntityFramework/ExtensionMethods.cs
+++ src/providers/WorkflowCore.Persistence.EntityFramework/ExtensionMethods.cs
@@ -13,8 +13,8 @@ namespace WorkflowCore.Persistence.EntityFramework
 
         internal static PersistedWorkflow ToPersistable(this WorkflowInstance instance, PersistedWorkflow persistable = null)
         {
-            if (persistable == null)            
-                persistable = new PersistedWorkflow();                        
+            if (persistable == null)
+                persistable = new PersistedWorkflow();
 
             persistable.Data = JsonConvert.SerializeObject(instance.Data, SerializerSettings);
             persistable.Description = instance.Description;
@@ -25,19 +25,19 @@ namespace WorkflowCore.Persistence.EntityFramework
             persistable.WorkflowDefinitionId = instance.WorkflowDefinitionId;
             persistable.Status = instance.Status;
             persistable.CreateTime = instance.CreateTime;
-            persistable.CompleteTime = instance.CompleteTime;            
-            
+            persistable.CompleteTime = instance.CompleteTime;
+
             foreach (var ep in instance.ExecutionPointers)
             {
                 var persistedEP = persistable.ExecutionPointers.FindById(ep.Id);
-                
+
                 if (persistedEP == null)
                 {
                     persistedEP = new PersistedExecutionPointer();
                     persistedEP.Id = ep.Id ?? Guid.NewGuid().ToString();
                     persistable.ExecutionPointers.Add(persistedEP);
-                }                 
-                
+                }
+
                 persistedEP.StepId = ep.StepId;
                 persistedEP.Active = ep.Active;
                 persistedEP.SleepUntil = ep.SleepUntil;
@@ -83,7 +83,7 @@ namespace WorkflowCore.Persistence.EntityFramework
 
         internal static PersistedExecutionError ToPersistable(this ExecutionError instance)
         {
-            var result = new PersistedExecutionError();            
+            var result = new PersistedExecutionError();
             result.ErrorTime = instance.ErrorTime;
             result.Message = instance.Message;
             result.ExecutionPointerId = instance.ExecutionPointerId;
@@ -94,7 +94,7 @@ namespace WorkflowCore.Persistence.EntityFramework
 
         internal static PersistedSubscription ToPersistable(this EventSubscription instance)
         {
-            PersistedSubscription result = new PersistedSubscription();            
+            PersistedSubscription result = new PersistedSubscription();
             result.SubscriptionId = new Guid(instance.Id);
             result.EventKey = instance.EventKey;
             result.EventName = instance.EventName;
@@ -102,11 +102,14 @@ namespace WorkflowCore.Persistence.EntityFramework
             result.ExecutionPointerId = instance.ExecutionPointerId;
             result.WorkflowId = instance.WorkflowId;
             result.SubscribeAsOf = DateTime.SpecifyKind(instance.SubscribeAsOf, DateTimeKind.Utc);
-            result.SubscriptionData = JsonConvert.SerializeObject(instance.SubscriptionData, SerializerSettings);
+            if (instance.SubscriptionData != null)
+            {
+                result.SubscriptionData = JsonConvert.SerializeObject(instance.SubscriptionData, SerializerSettings);
+            }
             result.ExternalToken = instance.ExternalToken;
             result.ExternalTokenExpiry = instance.ExternalTokenExpiry;
             result.ExternalWorkerId = instance.ExternalWorkerId;
-            
+
             return result;
         }
 
@@ -152,7 +155,7 @@ namespace WorkflowCore.Persistence.EntityFramework
 
             foreach (var ep in instance.ExecutionPointers)
             {
-                var pointer = new ExecutionPointer();                
+                var pointer = new ExecutionPointer();
 
                 pointer.Id = ep.Id;
                 pointer.StepId = ep.StepId;
