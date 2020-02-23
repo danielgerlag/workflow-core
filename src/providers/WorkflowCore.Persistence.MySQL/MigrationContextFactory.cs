5bf34b1a69d22056f5563e5884165f8a3106be32
Daniel Gerlag <daniel@gerlag.ca>
2020-02-23 07:51:13 -0800

    Update EF provider libraries (#520)

---
+++ src/providers/WorkflowCore.Persistence.MySQL/MigrationContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WorkflowCore.Persistence.MySQL
+{
+    public class MigrationContextFactory : IDesignTimeDbContextFactory<MysqlContext>
+    {
+        public MysqlContext CreateDbContext(string[] args)
+        {
+            return new MysqlContext(@"Server=127.0.0.1;Database=myDataBase;Uid=myUsername;Pwd=myPassword;");
+        }
+    }
+}
--- src/providers/WorkflowCore.Persistence.MySQL/Migrations/20200223041701_Activities.Designer.cs
+++ src/providers/WorkflowCore.Persistence.MySQL/Migrations/20200223041701_Activities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkflowCore.Persistence.MySQL;
 
 namespace WorkflowCore.Persistence.MySQL.Migrations
 {
     [DbContext(typeof(MysqlContext))]
-    partial class MysqlPersistenceProviderModelSnapshot : ModelSnapshot
+    [Migration("20200223041701_Activities")]
+    partial class Activities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
