edf616e3747aa4fcde924a9d14276b4c57160662
nkirsanov <n.kirsanov@cft.ru>
2020-02-25 17:32:31 +0700

    clean TryCatchSample

--- src/samples/WorkflowCore.Sample19/TryCatchWorkflow.cs
+++ src/samples/WorkflowCore.Sample19/TryCatchWorkflow.cs
@@ -12,20 +12,6 @@ namespace WorkflowCore.Sample19
 
         public void Build(IWorkflowBuilder<Data> builder)
         {
-            // builder.StartWith(_ => ExecutionResult.Next())
-            //     .Output(data => data.Message, step => "Custom Message")
-            //     .Try(b => b.StartWith(_ => throw new AggregateException("I am Exception1")))
-            //     .Catch(new[] {typeof(ArgumentException)},
-            //         ctx => Console.WriteLine($"Caught ArgumentException, message: {ctx.CurrentException.Message}"))
-            //     .Catch(new[] {typeof(Exception)},
-            // ctx => Console.WriteLine($"Caught Exception, message: {ctx.CurrentException.Message}"))
-            //     .Then<CustomMessage>(s => s.Input(msg => msg.Message, data => data.Message));
-
-            // builder.StartWith(_ => ExecutionResult.Next())
-            //     .Output(data => data.Message, step => "Custom Message")
-            //     .Try(b => b.StartWith(_ => throw new Exception("I am Exception1")))
-            //     .Catch<CustomMessage>(new[] {typeof(Exception)});
-
             builder.StartWith(_ => ExecutionResult.Next())
                 .Output(data => data.Message, step => "Custom Message")
                 .Try(b => b.StartWith(_ => throw new ArgumentException("I am Exception1")))
