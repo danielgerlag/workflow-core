dee17c27e0b18ed22293756b38bb1502546b4fb9
xavid.ramirez@apchomehealth.onmicrosoft.com <xavid.ramirez@apchomehealth.onmicrosoft.com>
2020-12-16 14:12:32 -0600

    Adding RavenDB persistence provider

---
+++ src/providers/WorkflowCore.Persistence.RavenDB/Services/RavenDbIndexes.cs
@@ -0,0 +1,14 @@
+﻿using Raven.Client.Documents.Indexes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkflowCore.Models;
+
+namespace WorkflowCore.Persistence.RavenDB.Services
+{
+	// TODO:  Implement Map for result bind of Index
+	public class WorkflowInstances_Id : AbstractIndexCreationTask<WorkflowInstance> { }
+	public class EventSubscriptions_Id : AbstractIndexCreationTask<EventSubscription> { }
+	public class Events_Id : AbstractIndexCreationTask<Event> { }
+	public class ExecutionErrors_Id : AbstractIndexCreationTask<ExecutionError> { }
+}
