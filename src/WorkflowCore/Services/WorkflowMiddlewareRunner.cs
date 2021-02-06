4760124ab334f65db255fd7a20e1211839613e4b
Mikhail Merkulov <mihail.merkulov@gmail.com>
2021-02-06 20:38:33 +0200

    Added workflow instance to error handler

--- src/WorkflowCore/Services/WorkflowMiddlewareRunner.cs
+++ src/WorkflowCore/Services/WorkflowMiddlewareRunner.cs
@@ -71,7 +71,7 @@ namespace WorkflowCore.Services
                     var typeInstance = scope.ServiceProvider.GetService(errorHandlerType);
                     if (typeInstance != null && typeInstance is IWorkflowMiddlewareErrorHandler handler)
                     {
-                        await handler.HandleAsync(exception);
+                        await handler.HandleAsync(workflow, exception);
                     }
                 }
             }
