cab5eae459beabb70ac0ea184b8552092de2dcd3
Wangyang Ding <wangyangding@microsoft.com>
2021-05-31 10:38:50 +0800

    update

--- test/WorkflowCore.TestAssets/DataTypes/CounterBoardWithDynamicData.cs
+++ test/WorkflowCore.TestAssets/DataTypes/CounterBoardWithDynamicData.cs
@@ -5,5 +5,6 @@ namespace WorkflowCore.TestAssets.DataTypes
     public class CounterBoardWithDynamicData: CounterBoard
     {
         public DynamicData DynamicDataInstance { get; set; }
+        public CounterBoard CounterBoardInstance { get; set; }
     }
 }
