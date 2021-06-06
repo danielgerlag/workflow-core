cae9537bed7f6cfbb689551941f8345e7e7104c6
praveenpaulose <praveen@celusion.com>
2021-06-06 14:27:22 +0530

    Add a new lock provider using MySql GET_LOCK and RELEASE_LOCK

---
+++ src/providers/WorkflowCore.LockProviders.MySQL/MySqlLockProvider.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using WorkflowCore.Interface;
+using System.Data;
+using System.Collections.Generic;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+namespace WorkflowCore.LockProviders.MySQL
+{
+    public class MySqlLockProvider : IDistributedLockProvider
+    {
+        private const string Prefix = "wfc";
+
+        private readonly string _connectionString;
+        private readonly ILogger _logger;
+        private readonly Dictionary<string, MySqlConnection> _locks = new Dictionary<string, MySqlConnection>();
+        private readonly AutoResetEvent _mutex = new AutoResetEvent(true);
+
+        public MySqlLockProvider(string connectionString, ILoggerFactory logFactory)
+        {
+            _logger = logFactory.CreateLogger<MySqlLockProvider>();
+            var csb = new MySqlConnectionStringBuilder(connectionString);
+            csb.Pooling = true;
+            _connectionString = csb.ToString();
+        }
+
+
+        public async Task<bool> AcquireLock(string Id, CancellationToken cancellationToken)
+        {
+            if (_mutex.WaitOne())
+            {
+                try
+                {
+                    var connection = new MySqlConnection(_connectionString);
+                    await connection.OpenAsync(cancellationToken);
+                    try
+                    {
+                        var cmd = connection.CreateCommand();
+                        cmd.CommandText = $"SELECT GET_LOCK('{Prefix}:{Id}', 10)";                                                
+                        
+                        var returnValue = await cmd.ExecuteScalarAsync(cancellationToken);
+
+                        if (returnValue == null)
+                        {
+                            _logger.LogError($"Acquire lock provider error for {Id}");
+                            return false;
+                        }
+
+                        var result = Convert.ToInt32(returnValue);                        
+                        if (result > 0)
+                        {
+                            _logger.LogDebug($"Acquired lock for {Id}");
+                            _locks[Id] = connection;
+                            return true;
+                        }
+                        else
+                        {
+                            _logger.LogError($"The acquire lock request timed out for {Id}");
+                            connection.Close();
+                            return false;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        connection.Close();
+                        throw ex;
+                    }
+                }
+                finally
+                {
+                    _mutex.Set();
+                }
+            }
+            return false;
+        }
+
+        public async Task ReleaseLock(string Id)
+        {
+            if (_mutex.WaitOne())
+            {
+                try
+                {
+                    MySqlConnection connection = null;
+
+                    if (_locks.ContainsKey(Id))
+                        connection = _locks[Id];
+                                        
+                    if (connection == null)
+                    {
+                        _logger.LogError($"Release lock connection not found for {Id}");
+                        return;
+                    }
+
+                    try
+                    {
+                        var cmd = connection.CreateCommand();
+                        cmd.CommandText = $"SELECT RELEASE_LOCK('{Prefix}:{Id}')";
+                        cmd.CommandType = CommandType.Text;                        
+
+                        var returnValue = await cmd.ExecuteScalarAsync();
+                        if (returnValue == null)
+                        {
+                            _logger.LogError($"Release lock provider error for {Id}");                            
+                        }
+
+                        var result = Convert.ToInt32(returnValue);
+
+                        if (result > 0)
+                        {
+                            _logger.LogDebug($"Released lock for {Id}");
+                        }
+                        else
+                        {
+                            _logger.LogError($"Release lock returned 0 for {Id}");
+                            
+                        }                        
+                    }
+                    finally
+                    {
+                        connection.Close();
+                        _locks.Remove(Id);
+                    }
+                }
+                finally
+                {
+                    _mutex.Set();
+                }
+            }
+        }
+
+        public Task Start() => Task.CompletedTask;
+        
+        public Task Stop() => Task.CompletedTask;
+        
+    }
+}
---
+++ src/providers/WorkflowCore.LockProviders.MySQL/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using WorkflowCore.LockProviders.MySQL;
+using WorkflowCore.Models;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static WorkflowOptions UseMySqlLocking(this WorkflowOptions options, string connectionString)
+        {
+            options.UseDistributedLockManager(sp => new MySqlLockProvider(connectionString, sp.GetService<ILoggerFactory>()));
+            return options;
+        }
+    }
+}
