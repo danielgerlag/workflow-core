133a7feac08e7a0cc4d6bd61d717429e01c9b7cd
Viktor Kryzhanovskyi <v-V.Kryzhanovskyi@lionbridge.com>
2021-08-10 12:04:07 +0300

    Changes after code review from Daniel

--- test/WorkflowCore.UnitTests/Services/LifeCycleEventPublisherTests.cs
+++ test/WorkflowCore.UnitTests/Services/LifeCycleEventPublisherTests.cs
@@ -1,8 +1,9 @@
-using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
 using WorkflowCore.Interface;
+using WorkflowCore.Models;
 using WorkflowCore.Models.LifeCycleEvents;
 using WorkflowCore.Services;
 using Xunit;
@@ -17,10 +18,17 @@ namespace WorkflowCore.UnitTests.Services
             // Arrange
             var wasCalled = new TaskCompletionSource<bool>();
             var eventHubMock = new Mock<ILifeCycleEventHub>();
+            var serviceCollectionMock = new Mock<IServiceCollection>();
+
+            var workflowOptions = new WorkflowOptions(serviceCollectionMock.Object)
+            {
+                EnableLifeCycleEventsPublisher = true
+            };
+
             eventHubMock
                 .Setup(hub => hub.PublishNotification(It.IsAny<StepCompleted>()))
                 .Callback(() => wasCalled.SetResult(true));
-            LifeCycleEventPublisher publisher = new LifeCycleEventPublisher(eventHubMock.Object, new LoggerFactory());
+            LifeCycleEventPublisher publisher = new LifeCycleEventPublisher(eventHubMock.Object, workflowOptions, new LoggerFactory());
 
             // Act
             publisher.Start();
@@ -28,6 +36,7 @@ namespace WorkflowCore.UnitTests.Services
 
             // Assert
             await wasCalled.Task;
+            eventHubMock.Verify(hub => hub.PublishNotification(It.IsAny<StepCompleted>()), Times.Once());
         }
 
         [Fact(DisplayName = "Notifications should be published when the publisher is running")]
@@ -36,10 +45,17 @@ namespace WorkflowCore.UnitTests.Services
             // Arrange
             var wasCalled = new TaskCompletionSource<bool>();
             var eventHubMock = new Mock<ILifeCycleEventHub>();
+            var serviceCollectionMock = new Mock<IServiceCollection>();
+
+            var workflowOptions = new WorkflowOptions(serviceCollectionMock.Object)
+            {
+                EnableLifeCycleEventsPublisher = true
+            };
+
             eventHubMock
                 .Setup(hub => hub.PublishNotification(It.IsAny<StepCompleted>()))
                 .Callback(() => wasCalled.SetResult(true));
-            LifeCycleEventPublisher publisher = new LifeCycleEventPublisher(eventHubMock.Object, new LoggerFactory());
+            LifeCycleEventPublisher publisher = new LifeCycleEventPublisher(eventHubMock.Object, workflowOptions, new LoggerFactory());
 
             // Act
             publisher.Start();
@@ -49,6 +65,33 @@ namespace WorkflowCore.UnitTests.Services
 
             // Assert
             await wasCalled.Task;
+            eventHubMock.Verify(hub => hub.PublishNotification(It.IsAny<StepCompleted>()), Times.Once());
+        }
+
+        [Fact(DisplayName = "Notifications should be disabled if option EnableLifeCycleEventsPublisher is disabled")]
+        public void PublishNotification_Disabled()
+        {
+            // Arrange
+            var eventHubMock = new Mock<ILifeCycleEventHub>();
+            var serviceCollectionMock = new Mock<IServiceCollection>();
+
+            var workflowOptions = new WorkflowOptions(serviceCollectionMock.Object)
+            {
+                EnableLifeCycleEventsPublisher = false
+            };
+
+            eventHubMock
+                .Setup(hub => hub.PublishNotification(It.IsAny<StepCompleted>()))
+                .Returns(Task.CompletedTask);
+            LifeCycleEventPublisher publisher = new LifeCycleEventPublisher(eventHubMock.Object, workflowOptions, new LoggerFactory());
+
+            // Act
+            publisher.Start();
+            publisher.PublishNotification(new StepCompleted());
+            publisher.Stop();
+
+            // Assert
+            eventHubMock.Verify(hub => hub.PublishNotification(It.IsAny<StepCompleted>()), Times.Never());
         }
     }
 }
