e77013106ca5b41ac6c6f841dc4b835efc3a5daa
glucaci <gabriel.lucaci.dev@gmail.com>
2022-11-22 15:41:06 +0100

    Consolidate tests projects dependencies

--- test/WorkflowCore.Tests.MySQL/DockerSetup.cs
+++ test/WorkflowCore.Tests.MySQL/DockerSetup.cs
@@ -1,46 +1,31 @@
-﻿using System.Collections.Generic;
-using Docker.Testify;
-using Xunit;
+﻿using Xunit;
 using System;
-using MySqlConnector;
+using System.Threading.Tasks;
+using Squadron;
 
 namespace WorkflowCore.Tests.MySQL
 {
-    public class MysqlDockerSetup : DockerSetup
+    public class MysqlDockerSetup : IAsyncLifetime
     {
+        private readonly MySqlResource _mySqlResource;
         public static string ConnectionString { get; set; }
         public static string ScenarioConnectionString { get; set; }
-        public static string RootPassword => "rootpwd123";
 
-        public override TimeSpan TimeOut => TimeSpan.FromSeconds(60);
-
-        public override string ImageName => "mysql";
-        public override IList<string> EnvironmentVariables => new List<string> {
-            $"MYSQL_ROOT_PASSWORD={RootPassword}"
-        };
-
-        public override int InternalPort => 3306;
-
-        public override void PublishConnectionInfo()
+        public MysqlDockerSetup()
         {
-            ConnectionString = $"Server=127.0.0.1;Port={ExternalPort};Database=workflow;User=root;Password={RootPassword};";
-            ScenarioConnectionString = $"Server=127.0.0.1;Port={ExternalPort};Database=scenarios;User=root;Password={RootPassword};";
+            _mySqlResource = new MySqlResource();
+        }
+        
+        public async Task InitializeAsync()
+        {
+            await _mySqlResource.InitializeAsync();
+            ConnectionString = _mySqlResource.ConnectionString;
+            ScenarioConnectionString = _mySqlResource.ConnectionString;
         }
 
-        public override bool TestReady()
+        public Task DisposeAsync()
         {
-            try
-            {
-                var connection = new MySqlConnection($"host=127.0.0.1;port={ExternalPort};user=root;password={RootPassword};database=mysql;");
-                connection.Open();
-                connection.Close();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-
+            return _mySqlResource.DisposeAsync();
         }
     }
 
@@ -48,5 +33,4 @@ namespace WorkflowCore.Tests.MySQL
     public class MysqlCollection : ICollectionFixture<MysqlDockerSetup>
     {
     }
-
 }
