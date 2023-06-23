1e41016cab72063d6441b1ec0cd6c5d6ef6eb644
Kar Martikyan <kar.m@plat.ai>
2023-06-23 14:56:45 +0400

    Injected IWorkflowDbContextFactory on UseSqlServer

--- src/providers/WorkflowCore.Persistence.SqlServer/ServiceCollectionExtensions.cs
+++ src/providers/WorkflowCore.Persistence.SqlServer/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using System.Data.Common;
 using WorkflowCore.Interface;
 using WorkflowCore.Models;
+using WorkflowCore.Persistence.EntityFramework.Interfaces;
 using WorkflowCore.Persistence.EntityFramework.Services;
 using WorkflowCore.Persistence.SqlServer;
 
@@ -13,6 +14,8 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             options.UsePersistence(sp => new EntityFrameworkPersistenceProvider(new SqlContextFactory(connectionString, initAction), canCreateDB, canMigrateDB));
             options.Services.AddTransient<IWorkflowPurger>(sp => new WorkflowPurger(new SqlContextFactory(connectionString, initAction)));
+            options.Services.AddTransient<IWorkflowDbContextFactory>(_ => new SqlContextFactory(connectionString, initAction));
+
             return options;
         }
     }
