c418c2376ffdf6b556483620a52ebdd07bed40c6
Chris Bertrand <cjb_bjc@hotmail.com>
2023-07-11 14:25:52 +0100

    Adding in logic to CreateEvent Method
    Adding in logic to CreateEvent Method

--- src/providers/WorkflowCore.Providers.Redis/Services/RedisPersistenceProvider.cs
+++ src/providers/WorkflowCore.Providers.Redis/Services/RedisPersistenceProvider.cs
@@ -182,8 +182,13 @@ namespace WorkflowCore.Providers.Redis.Services
             await _redis.HashSetAsync($"{_prefix}.{EVENT_SET}", newEvent.Id, str);
             await _redis.SortedSetAddAsync($"{_prefix}.{EVENT_SET}.{EVENTSLUG_INDEX}.{newEvent.EventName}-{newEvent.EventKey}", newEvent.Id, newEvent.EventTime.Ticks);
 
-            if (newEvent.IsProcessed)
+            if (newEvent.IsProcessed){
                 await _redis.SortedSetRemoveAsync($"{_prefix}.{EVENT_SET}.{RUNNABLE_INDEX}", newEvent.Id);
+                
+                if (_removeComplete){
+                    await _redis.SortedSetRemoveAsync($"{_prefix}.{EVENT_SET}", id);
+                }
+            }
             else
                 await _redis.SortedSetAddAsync($"{_prefix}.{EVENT_SET}.{RUNNABLE_INDEX}", newEvent.Id, newEvent.EventTime.Ticks);
 
