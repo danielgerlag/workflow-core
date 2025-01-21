5b9826915a0a36e0a77adecec8f7895131cbdafe
Edgardo Moreno <edgardo.moreno@redfarma.cl>
2025-01-21 13:02:35 -0300

    feat: add JSON serialization configuration and change SerializerSettings visibility to public
    - Added the `ConfigureJsonSettings` extension to customize JSON serialization settings in `WorkflowOptions`.
    - Changed `SerializerSettings` visibility from private to public to allow external access to Newtonsoft.Json configuration.
    
    Reason:
    When serializing objects like `Outcome` or `Steps`, some could not be serialized correctly. This change enables adjusting Newtonsoft.Json settings to meet specific requirements.

---
+++ src/providers/WorkflowCore.Persistence.EntityFramework/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Common;
+using Newtonsoft.Json;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+using WorkflowCore.Persistence.EntityFramework;
+using WorkflowCore.Persistence.EntityFramework.Services;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static WorkflowOptions ConfigureJsonSettings(this WorkflowOptions options, Action<JsonSerializerSettings> settings)
+        {
+            settings(ExtensionMethods.SerializerSettings);
+            return options;
+        }
+    }
+}
