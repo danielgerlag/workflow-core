041e7aa70dda4bbf5fdbee98ea250d0e75e66584
yi_Xu <yi_Xu@yixuju.cn>
2025-08-07 16:49:00 +0800

    fix: use timestamp with time zone in PostgreSQL

--- src/providers/WorkflowCore.Persistence.PostgreSQL/Migrations/20250807084543_ChangeDataTimeTypeForPostgreSQL.Designer.cs
+++ src/providers/WorkflowCore.Persistence.PostgreSQL/Migrations/20250807084543_ChangeDataTimeTypeForPostgreSQL.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WorkflowCore.Persistence.PostgreSQL;
@@ -11,9 +12,11 @@ using WorkflowCore.Persistence.PostgreSQL;
 namespace WorkflowCore.Persistence.PostgreSQL.Migrations
 {
     [DbContext(typeof(PostgresContext))]
-    partial class PostgresPersistenceProviderModelSnapshot : ModelSnapshot
+    [Migration("20250807084543_ChangeDataTimeTypeForPostgreSQL")]
+    partial class ChangeDataTimeTypeForPostgreSQL
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
